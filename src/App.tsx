3c3bcb1f085b52bbf7eb3d1f068228acca22f87d
Geferson Lopes <gefersonjefreey@gmail.com>
2025-08-20 21:34:23 -0300

    chore(deps): install react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
 
 import { ReactQueryProvider } from "./providers/ReactQueryProvider";
 import UseRoutes from "./routes/index.routes";
@@ -8,6 +9,7 @@ function App() {
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <ReactQueryProvider>
         <UseRoutes />
+        <ToastContainer />
       </ReactQueryProvider>
     </GoogleOAuthProvider>
   );
